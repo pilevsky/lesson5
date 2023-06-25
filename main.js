d73e08a21deb589fd5dae97240aa9680e6792893
pilevsky <pilevskiyd@yandex.ru>
2023-06-25 12:03:35 +0600

    Lesson 5

---
+++ main.js
@@ -0,0 +1,62 @@
+//                          Задание 1
+// let i = 0;
+// while (i <= 50){
+//     if(i != 0){
+//         console.log(i);
+//     }
+//     i++
+// }
+
+// for(let i = 0; i <= 50; i++){
+//     if(i != 0){
+//         console.log(i)
+//     }
+// }
+
+// let i = 0;
+// do {
+//     if(i != 0){
+//     console.log(i)
+//     }
+//     i++
+//     }
+// while(i <=50)
+//                          Задание 2
+// let i = 50;
+// while (i >= 25){
+//       console.log(i--);
+//     }
+
+// for(let i = 50; i >= 25; i--){
+//    console.log(i)
+//     }
+
+// let i = 50;
+// do {
+//     console.log(i)
+//     i--
+// }
+// while(i >= 25)
+//                         Задание 3
+// let i = 2500;
+// while (i <= 3000){
+//     if ((i % 2) != 0){
+//         console.log(i);
+// }
+// i++
+// }
+
+// for(let i = 2500; i <= 3000; i++){
+//     if((i % 2) != 0){
+//         console.log(i)
+//     }
+// }
+
+// let i = 2500;
+// do {
+//      if ((i % 2) != 0){
+//         console.log(i);
+// }
+// i++
+// }
+// while(i <= 3000)
